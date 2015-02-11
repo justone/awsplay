5936f29bf03259344eb62bec6e60fdec48b61787
Nate Jones <nate@endot.org>
2015-02-11 06:07:04 -0800

    lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to awsplay
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/awsplay/core_test.clj
@@ -0,0 +1,7 @@
+(ns awsplay.core-test
+  (:require [clojure.test :refer :all]
+            [awsplay.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
