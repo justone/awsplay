ecec4407d2a293bb4b374cbfeb1925051531a252
Nate Jones <nate@endot.org>
2015-02-11 21:20:12 -0800

    first (very rough) repl for playing with aws

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [amazonica "0.3.13"]
+                 [reply  "0.3.5" :exclusions [cheshire]]] ; exclude to prevent pulling in old jackson version
   :main ^:skip-aot awsplay.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/awsplay/core.clj
+++ src/awsplay/core.clj
@@ -1,7 +1,24 @@
 (ns awsplay.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.pprint :refer [pprint]]
+            [amazonica.core :refer :all]
+            [reply.main]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start up a repl."
   [& args]
-  (println "Hello, World!"))
+
+  ; don't wrap in the root!
+  (set-root-unwrapping! true)
+
+  ; import amazonica subs into the 'user' ns, because reply doesn't support setting the ns to this one
+  (create-ns 'user)
+  (in-ns 'user)
+  (require '[amazonica.aws.identitymanagement :refer :all])
+  (require '[amazonica.aws.s3 :refer :all])
+  (require '[amazonica.aws.ec2 :refer :all])
+
+  ; start the repl!
+  (reply.main/launch-standalone {
+                                 :value-to-string (fn [v] (with-out-str (clojure.pprint/pprint v)))
+                                 :print-err (fn [e] (clojure.pprint/pprint (if (instance? com.amazonaws.AmazonServiceException e) (ex->map e) e)))}))
